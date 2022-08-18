930efdbb40f365a38aeac8c4f4e143f3921784e6
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-18 13:04:23 +0530

    Test One Passed and PhLevelAnalyserTest

--- src/test/java/com/jap/PhLevelAnalyserTest.java
+++ src/test/java/com/jap/PhLevelAnalyserTest.java
@@ -2,16 +2,35 @@ package com.jap;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
-import static org.junit.Assert.*;
+
 
 public class PhLevelAnalyserTest {
+    PhLevelAnalyser phLevelAnalyser;
 
     @Before
     public void setUp()  {
+      phLevelAnalyser = new PhLevelAnalyser();
     }
 
     @After
     public void tearDown()  {
+        phLevelAnalyser = null;
     }
+    
+    @Test
+    public void getPhValueOfWaterForInputPh7 ( ) {
+        //arrange
+        String expectedResult ="pH value is fine";
+        //act
+        String actualResult = phLevelAnalyser.getPhValueOfWater( 7 );
+        //assert
+        Assertions.assertEquals(expectedResult,actualResult);
+    
+    }
+    
+   
+    
 }
